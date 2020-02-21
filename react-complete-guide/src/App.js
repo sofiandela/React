276883501dfed717da3ffde04442dfb7860386c4
Sofia Suvercase <ssuvercase@everfi.com>
2020-02-21 16:51:41 -0300

    Lists and Keys

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -14,27 +14,23 @@ class App extends Component {
   }
 
   //method
-  switchNameHandler = (newName) => {
-    //DONT DO THIS this.state.persons[0].name = "Maximilian";
-    this.setState({
-      persons: [
-        { name: newName, age: 28 },
-        { name: "Sofi", age: 24 },
-        { name: "Luis Gines ", age: 26 },
-
-      ]
-    })
-  }
-
   nameChangedHandler = (event) => {
     this.setState({
       persons: [
-        { name: "M ax", age: 28 },
+        { name: "Max", age: 28 },
         { name: "Sofi", age: 24 },
         { name: event.target.value, age: 26 },
 
       ]
     })
+  }
+
+  //queremos borrar una persona del array de personas
+  deletePersonHandler = (personIndex) => {
+    //const persons = this.state.persons.slice();
+    const persons = [...this.state.persons]
+    persons.splice(personIndex, 1);
+    this.setState({ persons: persons })
   }
 
 
@@ -53,39 +49,47 @@ class App extends Component {
       cursor: 'pointer'
     };
 
+    let persons = null;
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          {this.state.persons.map((person, index ) => {
+            return <Person
+              name={person.name}
+              age={person.age}
+              click={() => this.deletePersonHandler(index)}/>
+          })}  
+         </div> 
+      );
+    }
+
     return (
       <div className="App">
         <h1>Hi,  I am a React App</h1>
         {/* el bind mergea el estado viejo con el estado nuevo */}
         <button
           style={style}
-          onClick={this.togglePersonsHandler}>Toggle</button>
-        {
-          this.state.showPersons ?
-            <div>
-              <Person
-                name={this.state.persons[0].name}
-                age={this.state.persons[0].age}
-                click={this.switchNameHandler} />
-
-              <Person
-                name={this.state.persons[1].name}
-                age={this.state.persons[1].age} />
-
-              <Person
-                name={this.state.persons[2].name}
-                age={this.state.persons[2].age}
-                changed={this.nameChangedHandler} />
-            </div> : null
-        }
-
-
-
-
+          onClick={this.togglePersonsHandler}>Show Persons</button>
+          {persons}
       </div>
     );
-    //return React.createElement('div', {className: 'App'}, React.createElement('H1', null, 'Hi, I am a React App' ));
+    
   }
 }
 
 export default App;
+
+
+              // <Person
+              //   name={this.state.persons[0].name}
+              //   age={this.state.persons[0].age}
+              //   click={this.switchNameHandler} />
+
+              // <Person
+              //   name={this.state.persons[1].name}
+              //   age={this.state.persons[1].age} />
+
+              // <Person
+              //   name={this.state.persons[2].name}
+              //   age={this.state.persons[2].age}
+              //   changed={this.nameChangedHandler} />
